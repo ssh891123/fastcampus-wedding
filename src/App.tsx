dcdfb046e9adf7ec46fa17e5a98ac47aa99c68fd
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-05-30 22:20:38 +0900

    [feat] data fetch 구현
    - json server로 data fetch 구현
    - loading 중에는 heart 이미지에 animation 스타일 부여하여 심장이
      띄는것처럼 스타일링
    - fetching에서 에러발생하여 error을 throw하도록 함
      - error 연상하는 component가 렌더링하도록 표혐

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return <div className={cx('container')}>App</div>
+  const [wedding, setWedding] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // 1. wedding 데이터 호출
+  useEffect(() => {
+    setLoading(true)
+
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          setError(true)
+          throw new Error('청첩장 정보를 불러오지 못했습니다')
+        }
+        return response.json()
+      })
+      .then((value) => {
+        setWedding(value)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
