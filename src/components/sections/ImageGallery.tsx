374d38f6baa7d0d73bccfa9c75046fc69031f9ab
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-06-01 17:55:10 +0900

    [feat] image 캐러셀 구현
    - ImageViewer/index.tsx: swiper 라이브러리 이용하여 image 캐러셀 구현
    - ImageGallery: 이미지 갤러리에서 클릭하면 선택된 image로 화면이 전환되고, 캐러셀 동작하도록 구현
    - swiper.css: swiper 스타일을 override의 하여 화면 크기에 맞게 layout 스타일 적용

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,19 +1,47 @@
 import classNames from 'classnames/bind'
 import styles from './ImageGallery.module.scss'
 import Section from '@shared/Section'
+import ImageView from '@components/ImageViewer'
+import { useState } from 'react'
 
 const cx = classNames.bind(styles)
 
 export default function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIdx, setSelectedIdx] = useState(-1)
+
+  const open = selectedIdx > -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
+
   return (
-    <Section title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, idx) => (
-          <li key={idx} className={cx('wrap-image')}>
-            <img src={src} alt="사진첩 이미지" />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, idx) => (
+            <li key={idx} className={cx('wrap-image')}>
+              <img
+                src={src}
+                alt="사진첩 이미지"
+                onClick={() => {
+                  handleSelectedImage(idx)
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageView
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
+    </>
   )
 }
