81cf2dcc6437bdadffdb73e65494930e51355016
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-06-01 16:09:08 +0900

    [feat] video component 구현
    - 청첩장의 video 보여줄 component 구현
    - video tag를 이용하여 내부 component 구현

---
+++ src/components/sections/Video.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames/bind'
+import Section from '@shared/Section'
+import styles from './Video.module.scss'
+
+const cx = classNames.bind(styles)
+
+export default function Video() {
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
+}
