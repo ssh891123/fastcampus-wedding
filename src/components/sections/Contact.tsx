c9084d3ecc0bd2a65cae0698dc1258c33b65a99b
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-06-23 20:38:31 +0900

    [feat] contact component 추가
    - 용도: 연락처 및 축의금 내용 추가할 component

---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind'
+import styles from './Contact.module.scss'
+
+import Section from '@shared/Section'
+
+const cx = classNames.bind(styles)
+
+export default function Contact() {
+  return <Section title="연락처 및 마음 전하실 곳">Contact</Section>
+}
