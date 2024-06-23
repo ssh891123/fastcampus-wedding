13bbb5cb6a4a566b2946f5351e4d9384d151d33c
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-06-23 21:14:45 +0900

    [feat]Accordion Component 구현
    - content를 접었다 폇다 기능 추가
      - className을 스타일링 방식으로 content의 dispaly 스타일을 변경하여 기능 구현

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -2,9 +2,15 @@ import classNames from 'classnames/bind'
 import styles from './Contact.module.scss'
 
 import Section from '@shared/Section'
+import Accordion from '../shared/Accordion'
 
 const cx = classNames.bind(styles)
 
 export default function Contact() {
-  return <Section title="연락처 및 마음 전하실 곳">Contact</Section>
+  return (
+    <Section title="연락처 및 마음 전하실 곳">
+      <Accordion label="신랑측">신랑 신랑 신랑 신랑 신랑</Accordion>
+      <Accordion label="신부측">신부 신부 신부 신부 신부</Accordion>
+    </Section>
+  )
 }
