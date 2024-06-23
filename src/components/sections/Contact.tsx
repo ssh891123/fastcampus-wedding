1f908981797f9645433620717d25146e833c3bfd
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-06-23 22:41:39 +0900

    [feat] 계좌 복사 기능 추가
    - copy-to-clipboard 라이브러리 추가
    - 버튼 클릭시 clipboard에 업데이트 기능 추가

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,6 +5,7 @@ import Section from '@shared/Section'
 import Accordion from '@shared/Accordion'
 
 import { Person, Wedding } from '@models/wedding'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const cx = classNames.bind(styles)
 
@@ -71,7 +72,14 @@ function ContactInfo({ name, phoneNumber, account }: Person) {
           </a>
         </li>
         <li>
-          <button className={cx('button')}>복사</button>
+          <CopyToClipboard
+            text={`${account.bankName} ${account.accountNumber}`}
+            onCopy={() => {
+              alert('복사가 완료되었습니다.')
+            }}
+          >
+            <button className={cx('button')}>복사</button>
+          </CopyToClipboard>
         </li>
         {account.kakaopayLink ? (
           <li>
