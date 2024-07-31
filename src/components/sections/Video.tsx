8f45ee4ea5e33b9debbad229a0af7b88c2c0f3df
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-07-31 20:29:07 +0900

    [feat] 동영상 최적화
    1. 영상 길이 축소
    2. 영상 포맷 변경(mp4 -> webm)
    > www.medio.io 로 1, 2 방안 해보기

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -13,6 +13,7 @@ export default function Video() {
         loop={true}
         poster="/assets/poster.jpg"
       >
+        <source src="/assets/main.webm" type="video/webm" />
         <source src="/assets/main.mp4" type="video/mp4" />
       </video>
     </Section>
